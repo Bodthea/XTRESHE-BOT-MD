c5948f0d134a020045ee3926fc4ee3d75469ea9f
Bodthea <108167530+Bodthea@users.noreply.github.com>
2022-07-07 14:11:40 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,12 @@
 let handler =  m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [085713964963]
-│ • Telkomsel [081319944687]
+│ • smaftren Ooredoo [0881036447529]
+│ • Dana [0881036447529]
 ╰────
 
 ╭─「 Donasi • Non Pulsa 」
-│ • https://saweria.co/BochilGaming
-│ • Gopay [085713964963]
+│ • https://saweria.co/DheoID
+│ • Gopay [0881036447529]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
